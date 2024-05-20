144681e125a17e92ada4c29ffa4fb52dcd4750c5
Abdulrahman Oyawoye <abdulrahmanoyawoye@yahoo.com>
2024-05-20 14:39:43 -0700

    added something

--- project2.py
+++ project2.py
@@ -13,13 +13,16 @@ for i, char in enumerate(story):
         
     if char == target_end and start_of_word != -1:
         word = story[start_of_word: i + 1]
-        word = words.add(word)
+        words.add(word)
         start_of_word = -1
         
-answers = {"<name>": "Tim"}
+answers = {}
 
 for word in words:
     answer = input("Enter a word for" + word + ": ")
     answers[word] = answer
     
-print(answers)    
+for word in words :
+    story.replace(word, answers[word])
+    
+print(story)
