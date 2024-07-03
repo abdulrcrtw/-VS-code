822c387228cefc510a36ed949fb929ae99ce81ad
Abdulrahman Oyawoye <abdulrahmanoyawoye@yahoo.com>
2024-07-03 00:58:32 -0700

    added something

---
+++ Student.py
@@ -0,0 +1,9 @@
+class Student:
+    
+    def __init__(self, name, age, gpa, is_on_probation ):
+        self.name = name 
+        self.age = age
+        self.gpa = gpa
+        self.is_on_probation = is_on_probation
+        
+        
--- app.py
+++ app.py
@@ -1,3 +1,5 @@
-import Untitled3
+from Student import Student 
 
-print(Untitled3.roll_dice(10))
+Student1 = Student("Jim", "Seventeen", 2.8, False)
+Student2 = Student("Ella", "Twenty", 3.9, True)
+print(Student2.name)
