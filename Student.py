02c6085ce0e36497f0e1e7c226ad16783ff4cdd5
Abdulrahman Oyawoye <abdulrahmanoyawoye@yahoo.com>
2024-07-03 11:14:51 -0700

    added something

--- Student.py
+++ Student.py
@@ -1,9 +1,11 @@
 class Student:
-    
-    def __init__(self, name, age, gpa, is_on_probation ):
+    def __init__(self, name, major, gpa):
         self.name = name 
-        self.age = age
-        self.gpa = gpa
-        self.is_on_probation = is_on_probation
+        self.major = major
+        self.gpa = gpa 
         
-        
+    def on_honour_roll(self):
+        if self.gpa >= 3.5:
+            return True
+        else:
+            return False  
--- quiz.py
+++ quiz.py
@@ -16,6 +16,8 @@ def run_test(questions):
     score = 0
     for question in questions:
         answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
 print("You got " + str(score) + "/" + str(len(questions)) + " correct")
 
 run_test(questions)
