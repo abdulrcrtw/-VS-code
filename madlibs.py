e41ac266a10c7d09939496c0e437955e05e71ac9
Abdulrahman Oyawoye <abdulrahmanoyawoye@yahoo.com>
2024-07-04 09:09:18 -0700

    added something

---
+++ madlibs.py
@@ -0,0 +1,9 @@
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all thr time because I love to {verb1}. Stay hydrated and {verb2} like your are {famous_person}!"
+
+print(madlib)
