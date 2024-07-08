371fc170cbc5bf94bdfd2edae0277a8202d5f3a1
Abdulrahman Oyawoye <abdulrahmanoyawoye@yahoo.com>
2024-07-08 10:08:20 -0700

    added something

---
+++ test.py
@@ -0,0 +1,6 @@
+
+name = input("Enter your name: ")
+place = input("Enter a place of residence: ")
+age = input("Enter your age: ")
+country = input("Enter your country: ")
+print("Hello Mr " + name + " You live at " + place + " You are " + age + " years old, You are from " + country)
